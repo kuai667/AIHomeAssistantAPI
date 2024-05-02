424e5d58c81e5716f095cf0f18d3c52d1d539fc4
Luciano Dominguez <dominguezluciano336@gmail.com>
2024-05-02 13:53:20 +0200

    First commit

---
+++ AIHomeAssistantApi/Controllers/AssistantController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AIHomeAssistantApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AssistantController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult MainAction()
+        {
+            return Ok("The API is working");
+        }
+    }
+}
